f3163c5312c1076ea81eb4df55260480b843fca2
korenix <markkorenic@users.noreply.github.com>
2024-03-18 21:53:26 -0400

    update image

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <?php require_once('config.php'); ?>
 
 <html lang="en">
-<title> L.I.M.B.</title>
+<head>
+    <title> L.I.M.B.</title>
+</head>
+<link rel="stylesheet" href="css/style.css">
+
 <body style="background-color:beige;">
-<h2> L.I.M.B. <img src="limb.png" alt="limb" style="width:35px;height:35px;"></h2>
+<h2> L.I.M.B. <img src="limb3.png" alt="limb" style="width:35px;height:35px;"></h2>
 
 <div class="container">
     <label>Lookup Department:</label>
     <form action="search.php" method="post">
-      <input type="text" name="search"  placeholder="Department">
+      <input type="text" name="search" placeholder="Registrars" required >
     <br>
         <br>
         <input type="submit" value="Search">
@@ -16,6 +20,5 @@
 </div>
  <a href="dns.php">DNS Lookup</a> 
 <hr>
-<!-- maybe put search results here from search.php.-->
 </body>
 </html>
