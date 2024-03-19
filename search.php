7aa61deaeb7c3d331377dfe79b231fdb20ae7ed5
korenix <markkorenic@users.noreply.github.com>
2024-03-18 21:51:17 -0400

    Update search.php

--- search.php
+++ search.php
@@ -1,38 +1,43 @@
-<html lang="en">
-<body style="background-color:beige;">
+<?php include "templates/header.php" ?>
+<link rel="stylesheet" href="css/style.css">
 <a href="index.php"> Return to Search</a>
 <br>
 <br>
 <?php
+
 require_once('config.php');
+require("check_input.php");
+$search = check_input($_POST['search']);
+if (isset($_POST['search'])) {
+// 'search' relates to the form name in index.php, cool!
 
-$search = htmlspecialchars($_POST['search']);
-// 'search' relates to the form name in index.php!
+    $sql = "select * from local_it where department like '%$search%'";
+    //Same here!
 
-$sql = "select * from local_it where department like '%$search%'";
-//Same here!
-
-$result = mysqli_query($conn, $sql);
-printf("Search returned %d result(s) for '{$search}'\n", mysqli_num_rows($result));
-
-// TODO: Error checking here
-
+    if (isset($conn)) {
+        $result = mysqli_query($conn, $sql);
+        printf("Search returned %d result(s) for '$search'"."\n", mysqli_num_rows($result));
+        echo "<br>";
+    }
+}
 ?>
 <!-- Display search query from index.php ---------------------->
-  
 <style>table, th, td {
-    border:1px solid black;
-    }
-    </style>
-    <table style="width:inherit">
-        <tr>
-            <th>Department</th>
-            <th>Local IT</th>
-            <th>Resolution</th>
-            <!--<th>Action</th>-->
-        </tr>
+border:1px solid black;
+}
+</style>
+<table>
+    <thead>
+    <tr>
+        <th>Department</th>
+        <th>Local IT</th>
+        <th>Resolution</th>
+        <th>Updated</th>
 
+    </tr>
         <?php
+
+        if (isset($result)) {
         // fetch data based on search
         if ($result->num_rows > 0){
         while($row = $result->fetch_assoc() ){
@@ -41,17 +46,19 @@ printf("Search returned %d result(s) for '{$search}'\n", mysqli_num_rows($result
             <td><?php echo $row["department"];?></td>
             <td><?php echo $row["local_it"];?></td>
             <td><?php echo $row["resolution"];?></td>
-            <!--<td><a href=\"edit.php?id=$res[id]\"><button>Edit</button></a></td>-->
+            <td><?php echo $row["updated"];?></td>
         </tr>
             <?php
-    }
+        } // end while
+    } // end if
+}// end isset
+ else {
+     echo "0 records found";
+     if (isset($conn)) {
+         $conn->close();
+     }//end isset
+ }//end else
 
-} else {
-    echo "0 records found" ;
-
-}$conn->close();
 ?>
+</thead>
      </table>
-   </body>
-</html>
-
