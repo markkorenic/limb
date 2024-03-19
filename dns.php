7ab1200e9585df9390cccff6a24acb7ec84ee3c6
korenix <markkorenic@users.noreply.github.com>
2024-03-18 21:52:15 -0400

    refactoring header

--- dns.php
+++ dns.php
@@ -1,19 +1,27 @@
-<html lang="en">
-<body style="background-color:beige;">
-    <a href="index.php"> Return to L.I.M.B. Home</a>
+<?php include "templates/header.php" ?>
+<link rel="stylesheet" href="css/style.css">
+<a href="index.php">Return to L.I.M.B. Home</a>
 <br>
 <br>
-<h2> D.N.S.</h2>
-
+<h2>DNS Lookup</h2>
 <?php
-    $d = $_POST['domain'];
-    $result = dns_get_record($d, DNS_CNAME);
-    //print_r($result);
-    ?>
+/*---------------------------------------------------------
+*
+* put logic in check_input.php
+* This way, dns.php will look cleaner with mostly html
+*
+* ---------------------------------------------------------*/
+include("check_input.php");
+
+?>
 <div class="container">
     <label>Lookup Host:</label>
-    <form action="<?php echo ($_SERVER['PHP_SELF']); ?>" method="post">
-        <input type="text" name="domain"placeholder="my.unm.edu">
+
+    <form action="<?php check_input($_SERVER['PHP_SELF']); ?>" method="post">
+        <input type="text" name="domain" placeholder="my.unm.edu"> <br>
+        <span style="color:red;">* <?php if (!empty($website_err)) {
+                echo $website_err;
+            } ?></span>
         <br>
         <br>
         <input type="submit" value="Search">
@@ -24,11 +32,11 @@
         border:1px solid black;
     }
 </style>
-<table style="width:inherit">
-<tr>
-
 <?php
+if (!empty($result)){
 foreach($result as $dns) { ?>
+    <table>
+        <tr>
     <th>Domain</th>
     <th>Host</th>
 </tr>
@@ -38,11 +46,6 @@ foreach($result as $dns) { ?>
     </tr>
 <?php
 }
-
+}
 ?>
-
-
 </table>
-</body>
-</html>
-
